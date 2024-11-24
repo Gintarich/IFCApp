ab43680d2a2737a2ec4eab1c4a6cca9c592b6d6f
Gintarich <gintars.briedis@gmail.com>
2024-11-24 21:38:50 +0200

    Added matrix inversion and vector class

--- IFCApp.Core/Geometry/Matrix4d.cs
+++ IFCApp.Core/Geometry/Matrix4d.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace IFCApp.Core.Geometry;
@@ -48,7 +49,21 @@ public class Matrix4d
         });
     }
 
-    
+    ///<summary>
+    /// Rounds matrix to given decimal
+    ///</summary>
+    ///<param name="decimalCount"> Decimal count to round to</param>
+    public void Round(int decimalCount)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                _matrix[i, j] = Math.Round(_matrix[i, j], decimalCount);
+            }
+        }
+    }
+
     /// <summary>
     /// Creates a transformation matrix for rotation around the X-axis.
     /// </summary>
@@ -75,7 +90,7 @@ public class Matrix4d
     /// <returns>A transformation matrix for the specified rotation.</returns>
     public static Matrix4d RotationZ(double angle)
     {
-        var radians = angle * Math.PI / 180.0; 
+        var radians = angle * Math.PI / 180.0;
         double c = Math.Cos(radians);
         double s = Math.Sin(radians);
 
@@ -126,7 +141,7 @@ public class Matrix4d
 
     public Point3d Apply(Point3d point)
     {
-        var vArray = this.Apply([point.X,point.Y,point.Z]);
+        var vArray = this.Apply([point.X, point.Y, point.Z]);
         return new Point3d(vArray[0], vArray[1], vArray[2]);
     }
 
@@ -148,6 +163,109 @@ public class Matrix4d
         }
 
         return new Matrix4d(result);
+    }
+
+    /// <summary>
+    /// Calculates the inverse of the matrix if it exists.
+    /// </summary>
+    /// <returns>The inverse of the matrix as a new Matrix4d.</returns>
+    public Matrix4d Inverse()
+    {
+        double[,] m = _matrix; // Alias for readability
+
+        // Calculate determinant of the matrix
+        double det =
+            m[0, 3] * m[1, 2] * m[2, 1] * m[3, 0] - m[0, 2] * m[1, 3] * m[2, 1] * m[3, 0] -
+            m[0, 3] * m[1, 1] * m[2, 2] * m[3, 0] + m[0, 1] * m[1, 3] * m[2, 2] * m[3, 0] +
+            m[0, 2] * m[1, 1] * m[2, 3] * m[3, 0] - m[0, 1] * m[1, 2] * m[2, 3] * m[3, 0] -
+            m[0, 3] * m[1, 2] * m[2, 0] * m[3, 1] + m[0, 2] * m[1, 3] * m[2, 0] * m[3, 1] +
+            m[0, 3] * m[1, 0] * m[2, 2] * m[3, 1] - m[0, 0] * m[1, 3] * m[2, 2] * m[3, 1] -
+            m[0, 2] * m[1, 0] * m[2, 3] * m[3, 1] + m[0, 0] * m[1, 2] * m[2, 3] * m[3, 1] +
+            m[0, 3] * m[1, 1] * m[2, 0] * m[3, 2] - m[0, 1] * m[1, 3] * m[2, 0] * m[3, 2] -
+            m[0, 3] * m[1, 0] * m[2, 1] * m[3, 2] + m[0, 0] * m[1, 3] * m[2, 1] * m[3, 2] +
+            m[0, 1] * m[1, 0] * m[2, 3] * m[3, 2] - m[0, 0] * m[1, 1] * m[2, 3] * m[3, 2] -
+            m[0, 2] * m[1, 1] * m[2, 0] * m[3, 3] + m[0, 1] * m[1, 2] * m[2, 0] * m[3, 3] +
+            m[0, 2] * m[1, 0] * m[2, 1] * m[3, 3] - m[0, 0] * m[1, 2] * m[2, 1] * m[3, 3] -
+            m[0, 1] * m[1, 0] * m[2, 2] * m[3, 3] + m[0, 0] * m[1, 1] * m[2, 2] * m[3, 3];
+
+        if (Math.Abs(det) < 1e-9)
+            throw new InvalidOperationException("Matrix is not invertible.");
+
+        // Compute the inverse matrix (hardcoded for 4x4)
+        double[,] inv = new double[4, 4];
+
+        // Row 0
+        inv[0, 0] = (m[1, 2] * m[2, 3] * m[3, 1] - m[1, 3] * m[2, 2] * m[3, 1] +
+                     m[1, 3] * m[2, 1] * m[3, 2] - m[1, 1] * m[2, 3] * m[3, 2] -
+                     m[1, 2] * m[2, 1] * m[3, 3] + m[1, 1] * m[2, 2] * m[3, 3]) / det;
+
+        inv[0, 1] = (-m[0, 2] * m[2, 3] * m[3, 1] + m[0, 3] * m[2, 2] * m[3, 1] -
+                     m[0, 3] * m[2, 1] * m[3, 2] + m[0, 1] * m[2, 3] * m[3, 2] +
+                     m[0, 2] * m[2, 1] * m[3, 3] - m[0, 1] * m[2, 2] * m[3, 3]) / det;
+
+        inv[0, 2] = (m[0, 2] * m[1, 3] * m[3, 1] - m[0, 3] * m[1, 2] * m[3, 1] +
+                     m[0, 3] * m[1, 1] * m[3, 2] - m[0, 1] * m[1, 3] * m[3, 2] -
+                     m[0, 2] * m[1, 1] * m[3, 3] + m[0, 1] * m[1, 2] * m[3, 3]) / det;
+
+        inv[0, 3] = (-m[0, 2] * m[1, 3] * m[2, 1] + m[0, 3] * m[1, 2] * m[2, 1] -
+                     m[0, 3] * m[1, 1] * m[2, 2] + m[0, 1] * m[1, 3] * m[2, 2] +
+                     m[0, 2] * m[1, 1] * m[2, 3] - m[0, 1] * m[1, 2] * m[2, 3]) / det;
+
+        // Row 1
+        inv[1, 0] = (-m[1, 2] * m[2, 3] * m[3, 0] + m[1, 3] * m[2, 2] * m[3, 0] -
+                     m[1, 3] * m[2, 0] * m[3, 2] + m[1, 0] * m[2, 3] * m[3, 2] +
+                     m[1, 2] * m[2, 0] * m[3, 3] - m[1, 0] * m[2, 2] * m[3, 3]) / det;
+
+        inv[1, 1] = (m[0, 2] * m[2, 3] * m[3, 0] - m[0, 3] * m[2, 2] * m[3, 0] +
+                     m[0, 3] * m[2, 0] * m[3, 2] - m[0, 0] * m[2, 3] * m[3, 2] -
+                     m[0, 2] * m[2, 0] * m[3, 3] + m[0, 0] * m[2, 2] * m[3, 3]) / det;
+
+        inv[1, 2] = (-m[0, 2] * m[1, 3] * m[3, 0] + m[0, 3] * m[1, 2] * m[3, 0] -
+                     m[0, 3] * m[1, 0] * m[3, 2] + m[0, 0] * m[1, 3] * m[3, 2] +
+                     m[0, 2] * m[1, 0] * m[3, 3] - m[0, 0] * m[1, 2] * m[3, 3]) / det;
+
+        inv[1, 3] = (m[0, 2] * m[1, 3] * m[2, 0] - m[0, 3] * m[1, 2] * m[2, 0] +
+                     m[0, 3] * m[1, 0] * m[2, 2] - m[0, 0] * m[1, 3] * m[2, 2] -
+                     m[0, 2] * m[1, 0] * m[2, 3] + m[0, 0] * m[1, 2] * m[2, 3]) / det;
+
+
+        // Row 2
+        inv[2, 0] = (m[1, 1] * m[2, 3] * m[3, 0] - m[1, 3] * m[2, 1] * m[3, 0] +
+                     m[1, 3] * m[2, 0] * m[3, 1] - m[1, 0] * m[2, 3] * m[3, 1] -
+                     m[1, 1] * m[2, 0] * m[3, 3] + m[1, 0] * m[2, 1] * m[3, 3]) / det;
+
+        inv[2, 1] = (-m[0, 1] * m[2, 3] * m[3, 0] + m[0, 3] * m[2, 1] * m[3, 0] -
+                     m[0, 3] * m[2, 0] * m[3, 1] + m[0, 0] * m[2, 3] * m[3, 1] +
+                     m[0, 1] * m[2, 0] * m[3, 3] - m[0, 0] * m[2, 1] * m[3, 3]) / det;
+
+        inv[2, 2] = (m[0, 1] * m[1, 3] * m[3, 0] - m[0, 3] * m[1, 1] * m[3, 0] +
+                     m[0, 3] * m[1, 0] * m[3, 1] - m[0, 0] * m[1, 3] * m[3, 1] -
+                     m[0, 1] * m[1, 0] * m[3, 3] + m[0, 0] * m[1, 1] * m[3, 3]) / det;
+
+        inv[2, 3] = (-m[0, 1] * m[1, 3] * m[2, 0] + m[0, 3] * m[1, 1] * m[2, 0] -
+                     m[0, 3] * m[1, 0] * m[2, 1] + m[0, 0] * m[1, 3] * m[2, 1] +
+                     m[0, 1] * m[1, 0] * m[2, 3] - m[0, 0] * m[1, 1] * m[2, 3]) / det;
+
+        // Row 3
+        inv[3, 0] = (-m[1, 1] * m[2, 2] * m[3, 0] + m[1, 2] * m[2, 1] * m[3, 0] -
+                     m[1, 2] * m[2, 0] * m[3, 1] + m[1, 0] * m[2, 2] * m[3, 1] +
+                     m[1, 1] * m[2, 0] * m[3, 2] - m[1, 0] * m[2, 1] * m[3, 2]) / det;
+
+        inv[3, 1] = (m[0, 1] * m[2, 2] * m[3, 0] - m[0, 2] * m[2, 1] * m[3, 0] +
+                     m[0, 2] * m[2, 0] * m[3, 1] - m[0, 0] * m[2, 2] * m[3, 1] -
+                     m[0, 1] * m[2, 0] * m[3, 2] + m[0, 0] * m[2, 1] * m[3, 2]) / det;
+
+        inv[3, 2] = (-m[0, 1] * m[1, 2] * m[3, 0] + m[0, 2] * m[1, 1] * m[3, 0] -
+                     m[0, 2] * m[1, 0] * m[3, 1] + m[0, 0] * m[1, 2] * m[3, 1] +
+                     m[0, 1] * m[1, 0] * m[3, 2] - m[0, 0] * m[1, 1] * m[3, 2]) / det;
+
+        inv[3, 3] = (m[0, 1] * m[1, 2] * m[2, 0] - m[0, 2] * m[1, 1] * m[2, 0] +
+                     m[0, 2] * m[1, 0] * m[2, 1] - m[0, 0] * m[1, 2] * m[2, 1] -
+                     m[0, 1] * m[1, 0] * m[2, 2] + m[0, 0] * m[1, 1] * m[2, 2]) / det;
+
+
+
+        return new Matrix4d(inv);
     }
 
     // Override ToString() for better debugging output
---
+++ IFCApp.Core/Geometry/Vector3d.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IFCApp.Core.Geometry
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Vector3d"/> class.
+    /// </summary>
+    /// <param name="x">The X component of the vector.</param>
+    /// <param name="y">The Y component of the vector.</param>
+    /// <param name="z">The Z component of the vector.</param>
+    public struct Vector3d(double x = 0, double y = 0, double z = 0)
+    {
+        /// <summary>
+        /// Gets or sets the X component of the vector.
+        /// </summary>
+        public double X { get; set; } = x;
+
+        /// <summary>
+        /// Gets or sets the Y component of the vector.
+        /// </summary>
+        public double Y { get; set; } = y;
+
+        /// <summary>
+        /// Gets or sets the Z component of the vector.
+        /// </summary>
+        public double Z { get; set; } = z;
+
+        readonly public static Vector3d xAxis = new Vector3d(1, 0, 0);
+        readonly public static Vector3d yAxis = new Vector3d(0, 1, 0);
+        readonly public static Vector3d zAxis = new Vector3d(0, 0, 1);
+
+        /// <summary>
+        /// Calculates the magnitude (length) of the vector.
+        /// </summary>
+        /// <returns>The magnitude of the vector.</returns>
+        public double Length()
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+
+        /// <summary>
+        /// Returns a normalized version of the vector (unit vector).
+        /// </summary>
+        /// <returns>A new <see cref="Vector3d"/> representing the normalized vector.</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the vector is zero.</exception>
+        public Vector3d Normalize()
+        {
+            double magnitude = Length();
+            if (magnitude == 0)
+                throw new InvalidOperationException("Cannot normalize a zero vector.");
+            return new Vector3d(X / magnitude, Y / magnitude, Z / magnitude);
+        }
+
+        /// <summary>
+        /// Calculates the dot product of this vector with another vector.
+        /// </summary>
+        /// <param name="other">The other vector.</param>
+        /// <returns>The dot product as a double.</returns>
+        public double Dot(Vector3d other)
+        {
+            return X * other.X + Y * other.Y + Z * other.Z;
+        }
+
+        /// <summary>
+        /// Calculates the cross product of this vector with another vector.
+        /// </summary>
+        /// <param name="other">The other vector.</param>
+        /// <returns>A new <see cref="Vector3d"/> representing the cross product.</returns>
+        public Vector3d Cross(Vector3d other)
+        {
+            return new Vector3d(
+                Y * other.Z - Z * other.Y,
+                Z * other.X - X * other.Z,
+                X * other.Y - Y * other.X
+            );
+        }
+
+        /// <summary>
+        /// Adds two vectors.
+        /// </summary>
+        /// <param name="a">The first vector.</param>
+        /// <param name="b">The second vector.</param>
+        /// <returns>The resulting vector after addition.</returns>
+        public static Vector3d operator +(Vector3d a, Vector3d b)
+        {
+            return new Vector3d(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        /// <summary>
+        /// Subtracts one vector from another.
+        /// </summary>
+        /// <param name="a">The first vector.</param>
+        /// <param name="b">The second vector to subtract from the first.</param>
+        /// <returns>The resulting vector after subtraction.</returns>
+        public static Vector3d operator -(Vector3d a, Vector3d b)
+        {
+            return new Vector3d(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+
+        /// <summary>
+        /// Multiplies a vector by a scalar.
+        /// </summary>
+        /// <param name="a">The vector.</param>
+        /// <param name="scalar">The scalar value.</param>
+        /// <returns>The resulting vector after multiplication.</returns>
+        public static Vector3d operator *(Vector3d a, double scalar)
+        {
+            return new Vector3d(a.X * scalar, a.Y * scalar, a.Z * scalar);
+        }
+
+        /// <summary>
+        /// Multiplies a scalar by a vector.
+        /// </summary>
+        /// <param name="scalar">The scalar value.</param>
+        /// <param name="a">The vector.</param>
+        /// <returns>The resulting vector after multiplication.</returns>
+        public static Vector3d operator *(double scalar, Vector3d a)
+        {
+            return new Vector3d(a.X * scalar, a.Y * scalar, a.Z * scalar);
+        }
+
+        /// <summary>
+        /// Divides a vector by a scalar.
+        /// </summary>
+        /// <param name="a">The vector.</param>
+        /// <param name="scalar">The scalar value.</param>
+        /// <returns>The resulting vector after division.</returns>
+        /// <exception cref="DivideByZeroException">Thrown if the scalar is zero.</exception>
+        public static Vector3d operator /(Vector3d a, double scalar)
+        {
+            if (scalar == 0)
+                throw new DivideByZeroException("Cannot divide by zero.");
+            return new Vector3d(a.X / scalar, a.Y / scalar, a.Z / scalar);
+        }
+
+        /// <summary>
+        /// Returns a string representation of the vector.
+        /// </summary>
+        /// <returns>A string in the format "(X, Y, Z)".</returns>
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z})";
+        }
+    }
+}
