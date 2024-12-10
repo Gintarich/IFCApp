d165d30b1b31a936bae4bc2ac0e040073d28497f
Gintars Briedis <gintars.briedis@gmail.com>
2024-12-10 13:55:22 +0200

    Added doors and door service

---
+++ IFCApp.Core/Elements/Door.cs
@@ -0,0 +1,18 @@
+﻿using IFCApp.Core.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IFCApp.Core.Elements
+{
+    public class Door : Opening
+    {
+        public Door(BBox box, int fatherID = 0) : base(box, fatherID)
+        {
+        }
+
+        public Door(Point3d startPoint, Point3d endPoint, int fatherID = 0) : base(startPoint, endPoint, fatherID)
+        {
+        }
+    }
+}
---
+++ IFCApp.IFCServices/Services/IfcDoorService.cs
@@ -0,0 +1,40 @@
+﻿using IFCApp.Core.Elements;
+using IFCApp.IFCServices.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xbim.Ifc2x3.SharedBldgElements;
+
+namespace IFCApp.IFCServices.Services;
+
+public class IfcDoorService
+{
+    private IFCModel _model;
+    private TransformationService _transformationService;
+    private BBoxService _boxService;
+
+    public IfcDoorService(IFCModel model, TransformationService transformationService, BBoxService boxService)
+    {
+        _model = model;
+        _transformationService = transformationService;
+        _boxService = boxService;
+    }
+    public List<Door> GetDoor()
+    {
+        var doorsOut = new List<Door>();
+        var model = _model.GetModel();
+        var doors = model.Instances.OfType<IfcDoor>();
+
+        foreach (var door in doors)
+        {
+            var openingElement = door.FillsVoids.First().RelatingOpeningElement;
+            var tforms = _transformationService;
+            var matrix = tforms.GetTransformation(openingElement.ObjectPlacement);
+            BBoxService bBoxService = _boxService;
+            var box = bBoxService.GetBBox(openingElement.Representation, matrix).TrimBoxWidth(1000);
+            doorsOut.Add(new Door(box));
+        }
+        return doorsOut;
+    }
+}
