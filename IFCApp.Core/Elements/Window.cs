67eb7ae29f13ef621bdaf69cd69b3c08922f8307
Gintars Briedis <gintars.briedis@gmail.com>
2024-12-10 13:33:40 +0200

    Added aditional class for window

---
+++ IFCApp.Core/Elements/Window.cs
@@ -0,0 +1,18 @@
+﻿using IFCApp.Core.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IFCApp.Core.Elements
+{
+    public class Window : Opening
+    {
+        public Window(BBox box, int fatherID = 0) : base(box, fatherID)
+        {
+        }
+
+        public Window(Point3d startPoint, Point3d endPoint, int fatherID = 0) : base(startPoint, endPoint, fatherID)
+        {
+        }
+    }
+}
