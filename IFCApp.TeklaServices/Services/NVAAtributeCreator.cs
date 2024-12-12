54c13acc49da2c1ecb8a189daf343e87cdc7e268
Gintars Briedis <gintars.briedis@gmail.com>
2024-12-12 17:12:21 +0200

    added solution for attributes

---
+++ IFCApp.TeklaServices/Services/NVAAtributeCreator.cs
@@ -0,0 +1,77 @@
+﻿using IFCApp.TeklaServices.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tekla.Structures.Model;
+
+namespace IFCApp.TeklaServices.Services;
+
+public class NVAAtributeCreator
+{
+    public NVAAtributeCreator()
+    {
+    }
+    public void CreateAttributes(ModelObject mo)
+    {
+        if (mo is null) return;
+        switch (mo)
+        {
+            case Assembly ass:
+                CreateAttributesForAssembly(ass);
+                break;
+            case Part part:
+                CreateAttributesForPart(part);
+                break;
+        };
+        new Model().CommitChanges();
+    }
+
+    private void CreateAttributesForPart(Part pt)
+    {
+        string teklaMat = pt.GetStringProp("MATERIAL_TYPE");
+        if (teklaMat == "MISCELLANEOUS") teklaMat = pt.Material.MaterialString;
+        var material = string.Empty;
+        var name = pt.Name;
+        try
+        {
+            material = AttributeMapper.Material[teklaMat];
+        }
+        catch (KeyNotFoundException e)
+        {
+            throw new KeyNotFoundException($"The value that broke dict{teklaMat}");
+        }
+        pt.SetUserProperty("NOSAUKUMS", name);
+        pt.SetUserProperty("MATERIALS", material);
+        pt.Modify();
+    }
+
+    private void CreateAttributesForAssembly(Assembly ass)
+    {
+        var name = ass.Name;
+        string material = string.Empty;
+        if (!AttributeMapper.MaterialFromNames.TryGetValue(name, out material))
+        {
+            var mp = ass.GetMainPart() as Part;
+            material = mp.GetStringProp("MATERIAL_TYPE");
+            material = AttributeMapper.Material[material];
+        }
+        ass.SetUserProperty("NOSAUKUMS", name);
+        ass.SetUserProperty("MATERIALS", material);
+        ass.Modify();
+    }
+}
+
+public class AttributeMapper
+{
+    public static Dictionary<string, string> Material { get; set; } = new Dictionary<string, string>
+    {
+        { "CONCRETE", "DZELZSBETONS" },
+        { "STEEL", "TĒRAUDS" },
+        { "Insulation_hard", "IZOLĀCIJA" },
+        { "KOOLTHERM K20", "IZOLĀCIJA" }
+    };
+    public static Dictionary<string, string> MaterialFromNames { get; set; } = new Dictionary<string, string>
+    {
+        {"SIENAS PANELIS", "DZLEZSBETONA PANELIS AR SILTUMIZOLĀCIJU" }
+    };
+}
---
+++ IFCApp.Tests/TeklaTests/AttributeCreatorTests.cs
@@ -0,0 +1,30 @@
+﻿using IFCApp.TeklaServices.Services;
+using IFCApp.TeklaServices.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+
+namespace IFCApp.Tests.TeklaTests;
+
+[TestClass]
+public class AttributeCreatorTests
+{
+    [TestMethod]
+    public void ShouldCreateAttributes()
+    {
+        NVAAtributeCreator creator = new NVAAtributeCreator();
+        Model model = new Model();
+        var mos = model.GetModelObjectSelector();
+        var beams = mos.GetAllObjectsWithType(ModelObject.ModelObjectEnum.BEAM).ToList();
+        var plates = mos.GetAllObjectsWithType(ModelObject.ModelObjectEnum.CONTOURPLATE).ToList();
+        var assemblies = mos.GetAllObjectsWithType(ModelObject.ModelObjectEnum.ASSEMBLY).ToList();
+        var combo = beams.Concat(plates).Concat(assemblies);
+        foreach ( var part in combo)
+        {
+            creator.CreateAttributes(part);
+        }
+    }
+}
